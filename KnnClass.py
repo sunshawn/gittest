d543c6549e8c1d67a67ce4baeb42cd682bf4ecea
sunshawn <sunshawn@outlook.com>
2020-05-24 20:29:27 +0800

    增加注释及可读性

--- KnnClass.py
+++ KnnClass.py
@@ -8,8 +8,22 @@ from matplotlib import pyplot as plt
 
 
 class SHArtArgKNNClassifier(KNeighborsClassifier):
+    """
+    This class extends the KNNClassifier
+    This class can use the method 'my_artifi' to change the parameters artificially
+    when the accuracy or precision or recall is the most it returns it.
+    """
 
     def my_artifi(self, kneighbor, X, y, Xtest=np.array([]), ytest=np.array([])):
+        """
+        :param kneighbor: the k
+        :param X: the independent var of training set
+        :param y: the dependent var of training set
+        :param Xtest: the independent var of test set
+        :param ytest: the dependent var of test set
+        :return: k and it's state when the state is utmost
+        """
+
         # to ensure whether the test arrs is given
         try:
             print(Xtest[0])
@@ -17,14 +31,18 @@ class SHArtArgKNNClassifier(KNeighborsClassifier):
             Xtest = X
             ytest = y
 
-        # use accuracy now
+        # FixMe: use accuracy now
         rightl = []
         for i in range(1, kneighbor):
             self.n_neighbors = i
             self.fit(X, y)
             predict = self.predict(Xtest)
-            rights = (predict == ytest).mean()
+            rights = (predict == ytest).mean()  # the calculation of accuracy,
+            # equals rights = (predict == ytest).sum() / len(predict),
+            # that's because python can calculate True as 1 and False 0
             rightl.append(rights)
+            # TODO: to show the procedure
+
         plt.plot(np.array(range(1, kneighbor)), np.array(rightl))
         return rightl.index(max(rightl)) + 1, max(rightl)
 
