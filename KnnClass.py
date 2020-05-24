93dfe32b38bb3f90aef81ac82909408685f37ae7
sunshawn <sunshawn@outlook.com>
2020-05-24 10:24:02 +0800

    添加了一些些注释

--- KnnClass.py
+++ KnnClass.py
@@ -10,11 +10,14 @@ from matplotlib import pyplot as plt
 class ArtificialArgumentKNN(KNeighborsClassifier):
 
     def my_artifi(self, kneighbor, X, y, Xtest=np.array([]), ytest=np.array([])):
+		# to ensure whether the test arrays is given
         try:
             print(Xtest[0])
         except:
             Xtest = X
             ytest = y
+		
+		# use accuracy now
         rightl = []
         for i in range(1, kneighbor):
             self.n_neighbors = i
@@ -26,6 +29,7 @@ class ArtificialArgumentKNN(KNeighborsClassifier):
         return rightl.index(max(rightl)) + 1, max(rightl)
 
 
+# test the code
 if __name__ == '__main__':
     myknn = ArtificialArgumentKNN()
     x = np.array([[1, 3], [2, 8], [3, 5], [4, -1], [5, 5]])
